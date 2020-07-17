a516738d925a10c27bcdd9fcf9f6b317ddbcc26e
ajvalenz <67160144+ajvalenz@users.noreply.github.com>
2020-07-17 13:12:45 -0500

    agregando el juego

--- app/src/main/java/com/example/megacine/Juego.java
+++ app/src/main/java/com/example/megacine/Juego.java
@@ -4,11 +4,73 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Juego extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Random;
 
+public class Juego extends AppCompatActivity {
+    Dictionary<String,String>  juego = new Dictionary<String, String>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public Enumeration<String> keys() {
+            return null;
+        }
+
+        @Override
+        public Enumeration<String> elements() {
+            return null;
+        }
+
+        @Override
+        public String get(Object o) {
+            return null;
+        }
+
+        @Override
+        public String put(String s, String s2) {
+            return null;
+        }
+
+        @Override
+        public String remove(Object o) {
+            return null;
+        }
+    }
+  ArrayList preguntas = new ArrayList();
+  ArrayList respuestas= new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
+      //  preguntas.add(" ¿ Qué animal es la mascota de Jazmín en Aladdin?");
+        juego.put("¿ Qué animal es la mascota de Jazmín en Aladdin?", "Mono");
+        juego.put("¿Cómo se llama el actor protagonista de \" Solo en casa\"?","Macaulay Culkin");
+        juego.put("¿Cómo se llama el pájaro símbolo de los Juegos del Hambre?","Sinsajo");
+        juego.put("¿Qué personaje de Disney perdió su zapato de cristal ?","Cenicienta");
+        juego.put(" ¿Cómo se llama la serie de dibujos animados en la que sale un gato cósmico del siglo XXI?", "Doraemon");
+        Random rd= new Random();
+
+
+        int rCorrecta=0; //contador respeustas correctas
+        int rF=0; //contador respustas falladas
+        for(int x =0;x<=4;x++){
+                int i = rd.nextInt(5);
+              //  pregunta= list(juego.keys());
+
+
     }
+
+    }
+
+
 }
