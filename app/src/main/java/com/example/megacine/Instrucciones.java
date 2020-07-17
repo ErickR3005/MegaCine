83e92951908be30bd29305819ee17bb2c0d48b4a
ErickR3005 <59286962+ErickR3005@users.noreply.github.com>
2020-07-17 11:56:20 -0500

    Instrucciones

--- app/src/main/java/com/example/megacine/Instrucciones.java
+++ app/src/main/java/com/example/megacine/Instrucciones.java
@@ -2,7 +2,9 @@ package com.example.megacine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Instrucciones extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Instrucciones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrucciones);
+    }
+    public void iniciarJuego(View v){
+        startActivity(new Intent(this,Juego.class));
+    }
+    public void menuPrincipal(View v){
+        startActivity(new Intent(this,MainActivity.class));
     }
 }
