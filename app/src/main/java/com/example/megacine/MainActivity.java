0e45d8c9d0fe4f43151c95fbf8d38158f10acee3
ErickR3005 <59286962+ErickR3005@users.noreply.github.com>
2020-07-17 11:32:55 -0500

    Primera pantalla

--- app/src/main/java/com/example/megacine/MainActivity.java
+++ app/src/main/java/com/example/megacine/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.megacine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button empezarJuego=findViewById(R.id.btn_empezar);
+    Button instruccion=findViewById(R.id.btn_info);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //a
     }
 }
